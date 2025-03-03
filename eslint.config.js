8fc6f3555d1cfe1f627de8f2bc259342aafdf2d5
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2025-03-02 22:03:14 -0600

    Configure eslint prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import js from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
